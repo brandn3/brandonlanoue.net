cfa84228cedc8269f1dc0c21c2b2355c148063ed
brandn3 <brandon.lanoue03@gmail.com>
2023-12-04 20:09:05 -0500

    Version 1.0.0

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function SendUser(Location)
+{
+    switch (Location)
+    {
+       
+        case 0:
+            window.location.href = "CultivateSim.brandonlanoue.net";
+        break;
+
+    }
+}
+function GotoPage(Page)
+{
+    let Project = document.getElementById("Project");
+    let Blog = document.getElementById("Blog");
+    
+    if (Project && Blog != null)
+    {
+        if (Page == "Home")
+        {
+            Project.style.display = "block";
+            Blog.style.display = "none";
+        }
+        if (Page == "Blog")
+        {
+            Project.style.display = "none";
+            Blog.style.display = "block";
+        }
+    }
+}
